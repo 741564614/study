5ac842231279ca239c1edcec2899e645362bdc9e
cy <7415646414@qq.com>
2022-08-01 19:35:03 +0800

    0801每日一题

---
+++ offer/src/leetcode_1374.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class leetcode_1374 {
+    public static void main(String[] args) {
+        System.out.println(generateTheString(5));
+    }
+    public static String generateTheString(int n) {
+        char[] value=new char[n];
+        Arrays.fill(value,'a');
+        if((n&1)!=1){
+            value[n-1]='b';
+        }
+        return new String(value);
+    }
+}

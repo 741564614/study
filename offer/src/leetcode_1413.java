6a3e6cef8c099637a64975899d7a7755546fbb50
cy <7415646414@qq.com>
2022-08-09 22:22:23 +0800

    0809每日一题

---
+++ offer/src/leetcode_1413.java
@@ -0,0 +1,15 @@
+public class leetcode_1413 {
+    public static void main(String[] args) {
+        int[] nums=new int[]{2,3,5,-5,-1};
+        System.out.println(minStartValue(nums));
+    }
+    public static int minStartValue(int[] nums) {
+        int min = Integer.MAX_VALUE;
+        int sum=0;
+        for(int n:nums){
+            sum+=n;
+            min=Math.min(min,sum);
+        }
+        return min>0?1:Math.abs(min)+1;
+    }
+}

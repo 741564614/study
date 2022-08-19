b96d652a7e6746bd0eb3afaff99d4513fa1cb27e
cy <7415646414@qq.com>
2022-08-19 22:44:47 +0800

    0819每日一题

---
+++ offer/src/leetcode_1450.java
@@ -0,0 +1,15 @@
+public class leetcode_1450 {
+    public static void main(String[] args) {
+
+    }
+
+    public int busyStudent(int[] startTime, int[] endTime, int queryTime) {
+        int ans = 0;
+        for (int i = 0; i < startTime.length; i++) {
+            if (queryTime >= startTime[i] && queryTime <= endTime[i]) {
+                ans++;
+            }
+        }
+        return ans;
+    }
+}

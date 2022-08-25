743cc0f744c93b45b23cb9ba3f43cfcc2c69f980
cy <7415646414@qq.com>
2022-08-25 21:51:23 +0800

    20220825每日一题

--- offer/src/leetcode/leetcode202208/leetcode_658.java
+++ offer/src/leetcode/leetcode202208/leetcode_658.java
@@ -1,5 +1,7 @@
 package leetcode.leetcode202208;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -39,5 +41,22 @@ public class leetcode_658 {
             result.add(arr[p]);
         }
         return result;
+    }
+
+    public List<Integer> findClosestElements3(int[] arr, int k, int x) {
+        List<Integer> list = new ArrayList<Integer>();
+        for (int num : arr) {
+            list.add(num);
+        }
+        Collections.sort(list, (a, b) -> {
+            if (Math.abs(a - x) != Math.abs(b - x)) {
+                return Math.abs(a - x) - Math.abs(b - x);
+            } else {
+                return a - b;
+            }
+        });
+        List<Integer> ans = list.subList(0, k);
+        Collections.sort(ans);
+        return ans;
     }
 }

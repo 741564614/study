392066d28ad5e2085c2920f65968978bd769a528
cy <7415646414@qq.com>
2022-08-28 13:53:05 +0800

    20220828每日一题

---
+++ offer/src/leetcode/leetcode202208/leetcode_793.java
@@ -0,0 +1,33 @@
+package leetcode.leetcode202208;
+
+public class leetcode_793 {
+    public static void main(String[] args) {
+
+    }
+    public int preimageSizeFZF2(int k) {
+        return (int) (help(k + 1) - help(k));
+    }
+
+    public long help(int k) {
+        long r = 5L * k;
+        long l = 0;
+        while (l <= r) {
+            long mid = (l + r) / 2;
+            if (zeta(mid) < k) {
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+        return r + 1;
+    }
+
+    public long zeta(long x) {
+        long res = 0;
+        while (x != 0) {
+            res += x / 5;
+            x /= 5;
+        }
+        return res;
+    }
+}

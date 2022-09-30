488656315ae0af52898a9dc78d3cb97234c1fdd1
cy <7415646414@qq.com>
2022-09-30 19:43:08 +0800

    20220930每日一题

---
+++ offer/src/leetcode/leetcode202209/Interview_questions_01_08.java
@@ -0,0 +1,29 @@
+package leetcode.leetcode202209;
+
+public class Interview_questions_01_08 {
+    public static void main(String[] args) {
+
+    }
+
+    public void setZeroes(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        boolean[] row = new boolean[m];
+        boolean[] col = new boolean[n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    row[i] = true;
+                    col[j] = true;
+                }
+            }
+        }
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (row[i] || col[j]) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+}

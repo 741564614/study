2a656494f54c7998f159a38610fcb9cd0b0b74d8
cy <7415646414@qq.com>
2022-10-03 09:51:09 +0800

    20221003每日一题

---
+++ offer/src/leetcode/leetcode202209/leetcode_1784.java
@@ -0,0 +1,20 @@
+package leetcode.leetcode202209;
+
+public class leetcode_1784 {
+    public boolean checkOnesSegment(String s) {
+        int st=0;
+        while(st<s.length()&&s.charAt(st)=='1'){
+            st++;
+        }
+        for (int i = st; i < s.length(); i++) {
+            if(s.charAt(i)=='1'){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean checkOnesSegment02(String s) {
+        return !s.contains("01");
+    }
+}

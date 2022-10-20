4dbdc990aaf64c3bd40385ee479ec1422736bbbb
cy <7415646414@qq.com>
2022-10-20 20:20:20 +0800

    20221020每日一题

---
+++ offer/src/leetcode/leetcode202210/leetcode_779.java
@@ -0,0 +1,20 @@
+package leetcode.leetcode202210;
+
+public class leetcode_779 {
+    public static void main(String[] args) {
+        kthGrammar(3, 2);
+    }
+
+    public static int kthGrammar(int n, int k) {
+        boolean change = false;
+        while (k > 1) {
+            if (k % 2 != 0) {
+                k++;
+            } else {
+                change = !change;
+            }
+            k /= 2;
+        }
+        return change ? 1 : 0;
+    }
+}

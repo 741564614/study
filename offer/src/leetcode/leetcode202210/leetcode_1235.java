39893f4c1611293b0baa3dc04bec1d52aa8b4d4e
cy <7415646414@qq.com>
2022-10-22 07:45:25 +0800

    20221022每日一题

---
+++ offer/src/leetcode/leetcode202210/leetcode_1235.java
@@ -0,0 +1,34 @@
+package leetcode.leetcode202210;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class leetcode_1235 {
+    public int jobScheduling02(int[] startTime, int[] endTime, int[] profit) {
+        int n = startTime.length;
+        int[][] jobs = new int[n][3];
+        int[] dp = new int[n + 1];
+        for (int i = 0; i < n; i++) {
+            jobs[i] = new int[]{startTime[i], endTime[i], profit[i]};
+        }
+        Arrays.sort(jobs, Comparator.comparingInt(a -> a[1]));
+        for (int i = 1; i <= n; i++) {
+            int k = binarySearch(jobs, i - 1, jobs[i - 1][0]);
+            dp[i] = Math.max(dp[i - 1], dp[k] + jobs[i - 1][2]);
+        }
+        return dp[n];
+    }
+
+    public int binarySearch(int[][] jobs, int right, int target) {
+        int left = 0;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (jobs[mid][1] > target) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+}

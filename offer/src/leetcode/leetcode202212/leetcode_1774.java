5edd0ee98c26b0153fce1be218a79cdc729bed89
cy <7415646414@qq.com>
2022-12-04 13:50:57 +0800

    20221204每日一题

--- offer/src/leetcode/leetcode202212/leetcode_1774.java
+++ offer/src/leetcode/leetcode202212/leetcode_1774.java
@@ -23,4 +23,28 @@ public class leetcode_1774 {
             dfs(toppingCosts, i + 1, target, cost + 2 * toppingCosts[i]);
         }
     }
+
+    public static void main(String[] args) {
+        System.out.println(new leetcode_1774().closestCost02(new int[]{2, 3}, new int[]{4, 5, 100}, 18));
+    }
+
+    public int closestCost02(int[] base, int[] top, int V) {
+        boolean[] dp = new boolean[V + 5];
+        int ans = Integer.MAX_VALUE;
+        for (int x : base) { //判断只能选基料
+            if (x > V) ans = Math.min(ans, x);
+            else dp[x] = true;
+        }
+        for (int x : top) {
+            for (int i = 0; i < 2; i++) { //选2次
+                for (int j = V; j >= 0; j--) {
+                    if (dp[j] && j + x > V) ans = Math.min(ans, j + x); //计算大于的情况
+                    if (j > x) dp[j] |= dp[j - x];
+                }
+            }
+        }
+        //ans-V:目前获得与target的差距
+        for (int i = 0; i <= ans - V && i <= V; i++) if (dp[V - i]) return V - i;
+        return ans;
+    }
 }

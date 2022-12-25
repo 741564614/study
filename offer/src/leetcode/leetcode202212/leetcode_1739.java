6323f0fa50db40956c5507942375bdcd5a1eac88
cy <7415646414@qq.com>
2022-12-25 10:22:16 +0800

    20221225每日一题

---
+++ offer/src/leetcode/leetcode202212/leetcode_1739.java
@@ -0,0 +1,19 @@
+package leetcode.leetcode202212;
+
+public class leetcode_1739 {
+    public int minimumBoxes02(int n) {
+        int cur = 1, i = 1, j = 1;
+        while (n > cur) {
+            n -= cur;
+            i++;
+            cur += i;
+        }
+        cur = 1;
+        while (n > cur) {
+            n -= cur;
+            j++;
+            cur++;
+        }
+        return (i - 1) * i / 2 + j;
+    }
+}

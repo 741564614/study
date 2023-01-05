5cb59a312a926b83f27465415f21d836d9d047db
cy <7415646414@qq.com>
2023-01-05 23:10:03 +0800

    20230105每日一题

---
+++ offer/src/leetcode/leetcode202301/leetcode_1803.java
@@ -0,0 +1,69 @@
+package leetcode.leetcode202301;
+
+public class leetcode_1803 {
+    // 字典树的根节点
+    private Trie root = null;
+    // 最高位的二进制位编号为 14
+    private static final int HIGH_BIT = 14;
+
+    public int countPairs02(int[] nums, int low, int high) {
+        return f(nums, high) - f(nums, low - 1);
+    }
+
+    public int f(int[] nums, int x) {
+        root = new Trie();
+        int res = 0;
+        for (int i = 1; i < nums.length; i++) {
+            add(nums[i - 1]);
+            res += get(nums[i], x);
+        }
+        return res;
+    }
+
+    public void add(int num) {
+        Trie cur = root;
+        for (int k = HIGH_BIT; k >= 0; k--) {
+            int bit = (num >> k) & 1;
+            if (cur.son[bit] == null) {
+                cur.son[bit] = new Trie();
+            }
+            cur = cur.son[bit];
+            cur.sum++;
+        }
+    }
+
+    public int get(int num, int x) {
+        Trie cur = root;
+        int sum = 0;
+        for (int k = HIGH_BIT; k >= 0; k--) {
+            int r = (num >> k) & 1;
+            if (((x >> k) & 1) != 0) {
+                if (cur.son[r] != null) {
+                    sum += cur.son[r].sum;
+                }
+                if (cur.son[r ^ 1] == null) {
+                    return sum;
+                }
+                cur = cur.son[r ^ 1];
+            } else {
+                if (cur.son[r] == null) {
+                    return sum;
+                }
+                cur = cur.son[r];
+            }
+        }
+        sum += cur.sum;
+        return sum;
+    }
+}
+
+class Trie {
+    // son[0] 表示左子树，son[1] 表示右子树
+    Trie[] son = new Trie[2];
+    int sum;
+
+    public Trie() {
+        sum = 0;
+    }
+
+}

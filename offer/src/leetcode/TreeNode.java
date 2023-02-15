02f652c69fdb9385415b72fb968a2fce508374c5
cy <7415646414@qq.com>
2023-02-15 13:10:25 +0800

    新增treenode生成函数

--- offer/src/leetcode/TreeNode.java
+++ offer/src/leetcode/TreeNode.java
@@ -1,5 +1,8 @@
 package leetcode;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class TreeNode {
     public int val;
     public TreeNode left;
@@ -16,5 +19,27 @@ public class TreeNode {
         this.val = val;
         this.left = left;
         this.right = right;
+    }
+
+    public static TreeNode gen(int[] vals) {
+        TreeNode root = new TreeNode(vals[0]);
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        int index = 1;
+        while (!queue.isEmpty() && index < vals.length) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode poll = queue.poll();
+                if (index < vals.length) {
+                    poll.left = new TreeNode(vals[index++]);
+                    queue.offer(poll.left);
+                }
+                if (index < vals.length) {
+                    poll.right = new TreeNode(vals[index++]);
+                    queue.offer(poll.right);
+                }
+            }
+        }
+        return root;
     }
 }

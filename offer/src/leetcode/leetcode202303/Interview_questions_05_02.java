275dbebdd89b45cc9280da05efde802576cc350e
cy <7415646414@qq.com>
2023-03-02 09:28:22 +0800

    20230302每日一题

--- offer/src/leetcode/leetcode202303/Interview_questions_05_02.java
+++ offer/src/leetcode/leetcode202303/Interview_questions_05_02.java
@@ -3,6 +3,7 @@ package leetcode.leetcode202303;
 public class Interview_questions_05_02 {
     public String printBin(double num) {
         StringBuilder sb = new StringBuilder("0.");
+        //十进制小数转二进制，将小数部分乘2，取所得整数部分，不停循环直至小数部分为0
         while (num != 0) {
             if (sb.length() >= 32) {
                 return "ERROR";

ab1a2183354bf98d3ab42aca274a84998ea70e5e
cy <7415646414@qq.com>
2023-03-21 10:16:32 +0800

    20230321每日一题

---
+++ offer/src/leetcode/leetcode202303/leetcode_2469.java
@@ -0,0 +1,7 @@
+package leetcode.leetcode202303;
+
+public class leetcode_2469 {
+    public double[] convertTemperature(double celsius) {
+        return new double[]{celsius + 273.15D, celsius * 1.80 + 32.00};
+    }
+}

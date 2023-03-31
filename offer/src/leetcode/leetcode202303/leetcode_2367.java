c96aee2bd15e2f02fdf5d9f24df23a04a7995ffe
cy <7415646414@qq.com>
2023-03-31 11:08:33 +0800

    20230331每日一题

--- offer/src/leetcode/leetcode202303/leetcode_2367.java
+++ offer/src/leetcode/leetcode202303/leetcode_2367.java
@@ -15,4 +15,29 @@ public class leetcode_2367 {
         }
         return ans;
     }
+
+    class Solution {
+        public int arithmeticTriplets(int[] nums, int diff) {
+            int ans = 0;
+            int n = nums.length;
+            for (int i = 0, j = 1, k = 2; i < n - 2 && j < n - 1 && k < n; i++) {
+                j = Math.max(j, i + 1);
+                while (j < n - 1 && nums[j] - nums[i] < diff) {
+                    j++;
+                }
+                if (j >= n - 1 || nums[j] - nums[i] > diff) {
+                    continue;
+                }
+                k = Math.max(k, j + 1);
+                while (k < n && nums[k] - nums[j] < diff) {
+                    k++;
+                }
+                if (k < n && nums[k] - nums[j] == diff) {
+                    ans++;
+                }
+            }
+            return ans;
+        }
+    }
+
 }

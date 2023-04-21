9eea530efabae10e404c4de498c5f244ff7f9185
cy <7415646414@qq.com>
2023-04-21 11:04:05 +0800

    20230421每日一题

---
+++ offer/src/leetcode/leetcode202304/leetcode_2413.java
@@ -0,0 +1,10 @@
+package leetcode.leetcode202304;
+
+public class leetcode_2413 {
+    public int smallestEvenMultiple(int n) {
+        if(n%2==0){
+            return n;
+        }
+        return n*2;
+    }
+}

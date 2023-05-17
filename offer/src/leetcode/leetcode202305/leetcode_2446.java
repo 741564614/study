93421281b8eed2c8df3ab1bedb639cdc6fa94316
cy <7415646414@qq.com>
2023-05-17 12:41:37 +0800

    20230517每日一题

---
+++ offer/src/leetcode/leetcode202305/leetcode_2446.java
@@ -0,0 +1,10 @@
+package leetcode.leetcode202305;
+
+public class leetcode_2446 {
+    public boolean haveConflict(String[] event1, String[] event2) {
+        return (event1[0].compareTo(event2[0]) <= 0 && event1[1].compareTo(event2[0]) >= 0) ||
+                (event1[0].compareTo(event2[1]) <= 0 && event1[1].compareTo(event2[1]) >= 0) ||
+                (event1[0].compareTo(event2[0]) <= 0 && event1[1].compareTo(event2[0]) >= 0) ||
+                (event2[0].compareTo(event1[1]) <= 0 && event2[1].compareTo(event1[1]) >= 0);
+    }
+}

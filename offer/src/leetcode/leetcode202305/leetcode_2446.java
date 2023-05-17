08f6163b5db74c3c889173bf626448d405a342bf
cy <7415646414@qq.com>
2023-05-17 12:42:28 +0800

    20230517每日一题

--- offer/src/leetcode/leetcode202305/leetcode_2446.java
+++ offer/src/leetcode/leetcode202305/leetcode_2446.java
@@ -7,4 +7,11 @@ public class leetcode_2446 {
                 (event1[0].compareTo(event2[0]) <= 0 && event1[1].compareTo(event2[0]) >= 0) ||
                 (event2[0].compareTo(event1[1]) <= 0 && event2[1].compareTo(event1[1]) >= 0);
     }
+
+    class Solution {
+        public boolean haveConflict(String[] event1, String[] event2) {
+            return !(event1[1].compareTo(event2[0]) < 0 || event2[1].compareTo(event1[0]) < 0);
+        }
+    }
+
 }

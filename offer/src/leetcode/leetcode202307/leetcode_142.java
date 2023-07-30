416fdd45d165aa070784d7680e9040bbfd7386cf
cy <7415646414@qq.com>
2023-07-30 15:06:12 +0800

    20230730每日一题

---
+++ offer/src/leetcode/leetcode202307/leetcode_142.java
@@ -0,0 +1,23 @@
+package leetcode.leetcode202307;
+
+import leetcode.ListNode;
+
+public class leetcode_142 {
+    public ListNode detectCycle(ListNode head) {
+        ListNode slow = head, fast = head;
+        do {
+            if (fast == null || fast.next == null) {
+                return null;
+            }
+            slow = slow.next;
+            fast = fast.next.next;
+        } while (slow != fast);
+        slow = head;
+        while (slow != fast) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return slow;
+    }
+
+}

fef1484a82d102cb0679cf588b7541c411b74675
cy <7415646414@qq.com>
2023-08-14 15:55:42 +0800

    20230814每日一题

---
+++ offer/src/leetcode/leetcode202308/leetcode_617.java
@@ -0,0 +1,16 @@
+package leetcode.leetcode202308;
+
+import leetcode.TreeNode;
+
+public class leetcode_617 {
+    public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
+        if (root1 == null || root2 == null) {
+            return root1 == null ? root2 : root1;
+        }
+        TreeNode root = new TreeNode(root1.val + root2.val);
+        root.left = mergeTrees(root1.left, root2.left);
+        root.right = mergeTrees(root1.right, root2.right);
+        return root;
+    }
+
+}
